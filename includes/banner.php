945193237345380ec1ceb485b6f08158f273b5eb
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2017-04-12 18:45:38 -0300

    Alterações de includes

---
+++ includes/banner.php
@@ -0,0 +1,14 @@
+<section id="banner" class="major">
+	<div class="inner">
+		<header class="major">
+			<h1>Bem vindo(a) à Campos Maia</h1>
+		</header>
+		<div class="content">
+			<p>A responsive site template designed by HTML5 UP<br />
+			and released under the Creative Commons.</p>
+			<ul class="actions">
+				<li><a href="#one" class="button next scrolly">Get Started</a></li>
+			</ul>
+		</div>
+	</div>
+</section>
---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+<nav id="menu">
+	<ul class="links">
+		<li><a href="index.html">Home</a></li>
+		<li><a href="landing.html">Quem Somos</a></li>
+		<li><a href="generic.html">Áreas de Atuação</a></li>
+		<li><a href="elements.html">Publicações</a></li>
+	</ul>
+	<ul class="actions vertical">
+		<li><a href="#" class="button special fit">Contato</a></li>
+		<li><a href="#" class="button fit">Área de Clientes</a></li>
+	</ul>
+</nav>
